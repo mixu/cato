998352eb79a721577ebe6b314446945aed1b28f1
Mikito Takada <mikito.takada@gmail.com>
2013-04-02 19:08:42 -0700

    Move samples to separate repo

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Collection: require('./lib/collection.js'),
+  View: require('./lib/view.js'),
+  CollectionView: require('./lib/collection_view.js'),
+  Shim: require('./lib/shim.js'),
+
+  Server: require('./lib/server.js')
+};
--- samples/dialog-sample.js
+++
@@ -1,3 +0,0 @@
-// listens to an event from another view
-
-// variety of togglable subviews, very much like tabs (radio select => show different subview)
