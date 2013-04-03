9a8b7205f70aaa11d6240b6c18eda0038399f780
Mikito Takada <mikito.takada@gmail.com>
2013-04-02 17:55:55 -0700

    Start working on asset/package/state server

--- samples/server-sample.js
+++ samples/server-sample.js
@@ -2,43 +2,21 @@ var fs = require('fs'),
     url = require('url'),
     http = require('http');
 
-function getModule(pathname) {
+var vjsServer = require('../lib/server.js');
 
-}
-
-// Cases:
-// 1. assets: /assets/*
-// 2. API: /api/*
-// 3. app sections: (remainder)
-
-// find the module that corresponds to the current pathname
-
-// generate the base page (minimal wrapper, base CSS, framework core)
-
-// add a link to the module - via the packager API
-
-// possibilities:
-// 1. cold cache, cold DOM
-// 2. hot cache, cold DOM
-// 3. hot cache, hot DOM
-
+vjsServer.add('/', require('./table'));
 
 var server = http.createServer(function(req, res) {
   var pathname = url.parse(req.url).pathname;
-
-  switch(pathname) {
-    case '/':
-      res.setHeader('content-type', 'text/html');
-      res.end(fs.readFileSync('./public/index.html'));
-      break;
-    case '/css/style.css':
-      res.setHeader('content-type', 'text/css');
-      res.end(fs.readFileSync('./public/css/style.css'));
-      break;
-    default:
+  if(/^api\//.test(pathname)) {
+    // 1. API: /api/*
+  } else {
+    // 2. module page / module dynamic package / module static asset
+    if(!vjsServer.route(req, res)) {
       console.log('404', req.url);
       res.statusCode = 404;
       res.end();
+    }
   }
 });
 
