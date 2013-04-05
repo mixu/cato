5e57043a37e1e0602a335b20c81f8d31c14ae9d1
Mikito Takada <mikito.takada@gmail.com>
2013-04-04 19:15:18 -0700

    Make Outlet look more like a element, tag or binding, add annotation in Shim.el for fake DOM objects

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
   View: require('./lib/common/view.js'),
   CollectionView: require('./lib/common/collection_view.js'),
   Shim: require('./lib/shim.js'),
+  Outlet: require('./lib/common/outlet.js'),
 
   Server: require('./lib/server.js')
 };
